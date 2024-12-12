65a3542667d2f195523cd596f03626151bffebd0
Alassan Saine <assanlaye10@gmail.com>
2024-12-12 12:35:14 +0000

    first commit

--- src/main/java/com/inventorysystem/entity/Product.java
+++ src/main/java/com/inventorysystem/entity/Product.java
@@ -3,9 +3,6 @@ package com.inventorysystem.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "products")
 @Entity
 public class Product {
@@ -25,4 +22,55 @@ public class Product {
     @Column(name = "price", nullable = false)
     private double price;
 
+    public Product(){
+
+    }
+
+    public Product(Long id, String name, String description, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
